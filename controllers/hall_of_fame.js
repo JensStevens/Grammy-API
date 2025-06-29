9a119bb6cd0ef93e389b1559d8c074ab5900b094
JensStevens <stevens.jens@hotmail.com>
2025-06-29 16:58:56 +0200

    search query

--- controllers/hall_of_fame.js
+++ controllers/hall_of_fame.js
@@ -20,10 +20,37 @@ class HallOfFameController {
         .join("label as l", "hof.label_id = l.id")
         .build();
       const result = await req.db.prepare(sql).all();
-    //   console.log(result);
+      //   console.log(result);
       res.send(result);
     } catch (error) {
       console.error("Error fetching hall of fame data:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+  async searchArtist(req, res) {
+    try {
+      const sql = new SQLBuilder()
+        .select(
+          "hof.title",
+          "a.artist as artist",
+          "c.category as category",
+          "ind.year as inducted",
+          "rel.year as released",
+          "l.label as label"
+        )
+        .from("hall_of_fame as hof")
+        .join("artist as a", "hof.artist_id = a.id")
+        .join("category as c", "hof.category_id = c.id")
+        .join("inducted as ind", "hof.inducted_id = ind.id")
+        .join("released as rel", "hof.released_id = rel.id")
+        .join("label as l", "hof.label_id = l.id")
+        .where("a.artist", "LIKE")
+        .build();
+      console.log(sql);
+      const result = await req.db.prepare(sql).all(`%${req.query.artist}%`);
+      res.send(result);
+    } catch (error) {
+      console.error("Error searching artist data:", error);
       res.status(500).send("Internal Server Error");
     }
   }
--- routes/hall_of_fame.js
+++ routes/hall_of_fame.js
@@ -4,6 +4,6 @@ import HallOfFameController from "../controllers/hall_of_fame.js";
 const router = express.Router();
 
 router.get("/hall_of_fame", HallOfFameController.get);
-// router.get("/hall_of_fame/:id", HallOfFame);
+router.get("/hall_of_fame/search", HallOfFameController.searchArtist);
 
 export default router;
